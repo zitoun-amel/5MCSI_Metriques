44020e39e0e7c03cf97b8ab233f97f06ac481428
zitoun-amel <amel.zitoun21@gmail.com>
2025-12-03 11:01:03 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,11 +4,12 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-
+                                                                                                                                       
+app = Flask(__name__)                                                                                                                  
+                                                                                                                                       
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #Comm   
-    
-
+    return render_template('hello.html') #Comm
+  
 if __name__ == "__main__":
   app.run(debug=True)
