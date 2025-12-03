4843f8de8099dc30467dba8a3e7b0c7c3dc24620
zitoun-amel <amel.zitoun21@gmail.com>
2025-12-03 10:33:31 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,18 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
+
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
                                                                                                                                        
 app = Flask(__name__)  
 @app.route("/contact/")
