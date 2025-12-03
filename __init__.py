02e46ce74a6f0cf06c7da6be3f290a8c8cd5846f
zitoun-amel <amel.zitoun21@gmail.com>
2025-12-03 10:38:47 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,16 +4,15 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
+                                                                                         
+@app.route('/')
+def hello_world():
 
-                                                                                                
 app = Flask(__name__)  
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
-                                                                                                                                       
-@app.route('/')
-def hello_world():
-
+                                              
    return render_template('hello.html') #Comm2
   
 if __name__ == "__main__":
