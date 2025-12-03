52750580b111947302aadf69dd1bc72e5815186e
zitoun-amel <amel.zitoun21@gmail.com>
2025-12-03 10:56:59 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -24,3 +24,6 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+
+if __name__ == "__main__":
+  app.run(debug=True)
